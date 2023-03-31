cc3841d116e85a1d7a97423a0141ad2cb18e1767
Paula28 <paulalopezramirez28@outlook.es>
2023-03-31 20:50:55 +0100

    createHome, logout and userName in Navbar

---
+++ src/components/FormLogin/index.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import { useHistory } from "react-router-dom";
+import { useDispatch } from 'react-redux'
+import { setLoggedUser } from '../../redux/actions/postsActions';
+import { getAllUsers } from '../../services'
+
+function FormLogin() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const [user, setUser] = useState("")
+    const [userList, setUserList] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [showError, setShowError] = useState(false)
+
+    useEffect(() => {
+        if (!loading && userList.length === 0) {
+            init()
+        }
+    }, [])
+
+    const init = async () => {
+        setLoading(true)
+        const res = await getAllUsers();
+        setUserList(res?.data)
+        setLoading(false)
+    }
+
+    const handleClick = () => {
+        setLoading(true)
+        setShowError(false)
+        if (userList?.length > 0) {
+            const userFound = userList.filter((userObj) => userObj.username.toLowerCase() === user || userObj.email.toLowerCase() === user)
+            if (userFound.length > 0) {
+                dispatch(setLoggedUser(userFound[0]))
+                history.push('/main')
+            } else {
+                setShowError(true)
+            }
+        }
+        setLoading(false)
+    }
+
+    const handleKeyPressed = (e) => {
+        if (e.keyCode === 13) {
+            handleClick()
+        }
+    }
+
+    return (
+        <div className='container-form-login'>
+            <img src='img/logo.png' />
+            <span>Insert your mail or username</span>
+            <input type="text" onKeyDown={handleKeyPressed} onChange={(e) => setUser(`${e.target.value || ""}`.trim().toLowerCase())} value={user} />
+            {
+                showError && <span className='login-error'>No match found.</span>
+            }
+            <button disabled={loading} onClick={handleClick}>Login</button>
+        </div>
+    )
+}
+
+export default FormLogin
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,32 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
 import { useHistory } from "react-router-dom";
+import { setLoggedUser } from '../../redux/actions/postsActions';
 
 function Navbar() {
-    const history = useHistory();
+  const dispatch = useDispatch()
+  const history = useHistory();
+  const loggedUser = useSelector((state) => state.posts.loggedUser)
 
-    const handleClick = () => {
-        history.push(`/`);
-    }
+  const handleClick = () => {
+    history.push(`/main`);
+  }
+
+  const logout = () => {
+    dispatch(setLoggedUser(null))
+    history.push('/login')
+  }
+
   return (
     <div className="navbar">
       <div>
-        <img src='img/logo.png' onClick={handleClick}/>
+        <img src='/img/logo.png' onClick={handleClick} />
         {/* enlaces y loco */}
       </div>
-      <div>
-        {/* user */}
+      <div className='user-info'>
+        <span>{loggedUser?.username} - {loggedUser?.email}</span>
+        <img src='/img/logout.png' onClick={logout} />
       </div>
     </div>
   )
